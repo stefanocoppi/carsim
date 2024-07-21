dc947328cd6504db5dde8b29e96e9342165fe7ef
MKinght73 <stefano.coppi@gmail.com>
2024-07-21 13:32:51 +0200

    Auto - Sospensioni
    Calcolo forze verticali di molla ed ammortizzatore

--- scripts/raycast_suspension.gd
+++ scripts/raycast_suspension.gd
@@ -2,13 +2,25 @@ class_name RayCastSuspension
 extends RayCast3D
 
 # parametri delle sospensioni
-var spring_length = 0.2
+var spring_length = 0.1
 var tire_radius = 0.31
+var spring_stiffness = 125 #45.0
+var bump = 0.5 #3.5
+var rebound = 1.0 #4.0
 
+
+# variabili delle sospensioni
 var spring_curr_length: float = spring_length
+var spring_load_mm: float = 0.0
+var prev_spring_load_mm:float = 0.0
+var spring_load_newton:float = 0.0
+var spring_speed_mm_per_seconds:float = 0.0
+
+# parametri delle gomme
+var y_force: float = 0.0
 
 @onready var wheel_mesh = $MeshInstance3D
-
+@onready var car = $'..' # ottiene il nodo padre
 
 
 func _ready():
@@ -31,3 +43,36 @@ func apply_forces(delta):
 		spring_curr_length = get_collision_point().distance_to(global_transform.origin) - tire_radius
 	else:
 		spring_curr_length = spring_length
+	
+	# calcola la forza della molla
+	
+	# calcolo la compressione della molla in mm
+	spring_load_mm = (spring_length - spring_curr_length) * 1000
+	print("spring_load_mm=%s" % spring_load_mm)
+	
+	# calcola la forza della molla in N (Legge di Hooke)
+	spring_load_newton = spring_load_mm * spring_stiffness
+	print("spring_load_newton=%s" % spring_load_newton)
+	
+	# calcola la velocità di movimento della sospensione in mm per sec
+	spring_speed_mm_per_seconds = (spring_load_mm - prev_spring_load_mm) / delta
+	print("spring_speed_mm_per_seconds=%s" % spring_speed_mm_per_seconds)
+	prev_spring_load_mm = spring_load_mm
+	
+	# calcola la forza di smorzamento in N e la addiziona a spring_load
+	if spring_speed_mm_per_seconds >= 0:
+		spring_load_newton += spring_speed_mm_per_seconds * bump # bump
+	else :
+		spring_load_newton += spring_speed_mm_per_seconds * rebound # rebound
+	
+	y_force = spring_load_newton
+	y_force = max(0, y_force)
+	print("y_force=%s" % y_force)
+	
+	# applica le forze allo chassis dell'auto
+	if is_colliding():
+		
+		var contact = get_collision_point() - car.global_transform.origin
+		var normal = get_collision_normal()
+		
+		car.apply_force(normal * y_force, contact)
